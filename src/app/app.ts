bf2e72d789a7f41de9f2b9eb197d91360ffad432
Daniel Mihai <danielmihai@Daniels-Laptop.powerhub>
2025-11-26 20:22:56 +0000

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -16,7 +16,7 @@ const app = express();
 // Middlewares
 app.use(helmet());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
